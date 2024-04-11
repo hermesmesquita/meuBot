c2bd8fd1113ca25cbfdd65a27935dc12a9f882f5
Hermes <hermes@desktop.local>
2024-04-11 07:39:43 -0300

    Adicionando Pexels

---
+++ auth.js
@@ -0,0 +1,5 @@
+import { createClient } from 'pexels';
+
+const client = createClient('g70qmNzrYxw66AG2Aei4aXQe8ipOl6e6xDc40oD1yMMLk0uwliADSROT');
+
+// All requests made with the client will be authenticated
